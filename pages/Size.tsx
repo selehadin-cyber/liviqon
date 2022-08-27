a58235759e0274d23c4d3581cae30d3be26d6b2d
selehadin-cyber <sele4416@gmail.com>
2022-08-27 15:02:51 +0300

    Add support for floats

--- pages/Size.tsx
+++ pages/Size.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 const Size = () => {
-    const [width, setWidth] = useState(0)
-    const [length, setLength] = useState(0)
+    const [width, setWidth] = useState("")
+    const [length, setLength] = useState("")
     const [wininches, setWininches] = useState(0)
     const [lininches, setLininches] = useState(0)
 
@@ -11,15 +11,15 @@ const Size = () => {
         return Math.round(inches * 10) / 10
     }
     useEffect(() => {
-      setLininches(convert(length))
-      setWininches(convert(width))
+      setLininches(convert(parseFloat(length)))
+      setWininches(convert(parseFloat(width)))
     }, [width, length])
     
   return (
     <>
         <div>Size</div>
-        <input className='border border-black' value={width} onChange={(e) => setWidth(parseInt(e.target.value))} type="text" />
-        <input className='border border-black' value={length} onChange={(e) => setLength(parseInt(e.target.value))} type="text" />
+        <input className='border border-black' value={width} onChange={(e) => setWidth(e.target.value)} type="text" />
+        <input className='border border-black' value={length} onChange={(e) => setLength(e.target.value)} type="text" />
         <p>{wininches} x {lininches} Inches ( {width} x {length} cm)</p>
     </>
     
